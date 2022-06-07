b3bbd61bd7adcdccfa7e0df36cedeecce864cc63
Daniel Marks <profdc9@gmail.com>
2022-06-07 12:13:59 -0500

    Added code to firmware for bank select

--- firmware/Firmware.asm
+++ firmware/Firmware.asm
@@ -22,11 +22,16 @@
   ; readability
 
 ;ID bytes for booting and drive detection
+idbytes:
     cpx  #$20    ;ID bytes for ProDOS and the
     ldy  #$00    ; Apple Autostart ROM
     cpx  #$03    ;
-    cpx  #$3C    ;this one for older II's
-
+    ldx  #$3C    ;this one for older II's
+    bne  boot
+jumpbank:
+    sta  $bffb,x ; this is called to switch between rom banks
+    ldy  #0      ; try booting again
+boot:
     sty  buflo      ;zero out block numbers and buffer address
     sty  blklo
     sty  blkhi
@@ -43,14 +48,36 @@
     asl  a
     asl  a
     sta  unit       ; store unit number so Arduino knows what to load
-    
+    ora  #$88        ; add $88 to it so we can address from page $BF ($BFF8-$BFFB)
+                     ; this works around 6502 phantom read
+    tax
+
+    lda  #$FA        ; set register A control mode to 2
+    sta  $BFFB,x     ; write to 82C55 mode register (mode 2 reg A, mode 0 reg B)
+
     ldy	 #<msg
 writemsg:
     lda	 (buflo),y
-    sta	 $7D0-<msg,y
-    beq  pushadr
+    beq  dowait
+    sta	 $6D0-<msg,y
     iny
     bne  writemsg
+
+dowait:
+    ldy  #10
+waitkey:
+    lda  #0         ; wait a little
+	jsr  $fca8      ; do the wait
+    lda  $c000      ; do we have a key
+    bpl  nokey
+    sta  $c010
+    cmp  #13+128    ; see if its an enter key
+    bne  nokey
+    lda  #1         ; change ROM bank address line 1
+    bne  jumpbank 
+nokey:
+    dey
+    bne  waitkey
 
 pushadr:
     lda  #$08       ; push return address on stack
@@ -71,8 +98,7 @@ start:
                      ; this works around 6502 phantom read
     tax
 
-    lda  $BFFB,x     ; set register A control mode to 2
-    ora  #$c0    
+    lda  #$FA        ; set register A control mode to 2
     sta  $BFFB,x     ; write to 82C55 mode register (mode 2 reg A, mode 0 reg B)
 
     ldy	 #$ff        ; lets send the command bytes directly to the Arduino
@@ -158,16 +184,118 @@ waitwrite:
 .endmacro
 
 msg:   aschi   "DAN ][ PRESS ENTER"
-end:
+endmsg:
 .byte    0
 
 ; These bytes need to be at the top of the 256 byte firmware as ProDOS
 ; uses these to find the entry point and drive capabilities
 
-.repeat	251-<end
+.repeat	251-<endmsg
 .byte 0
 .endrepeat
 
 .byte   $00,$00  ;0000 blocks = check status
 .byte   $A7      ;bit 0=read 1=status
 .byte  <start    ;low byte of entry
+
+; this starts at $c700, page 2
+
+page2:
+;ID bytes for booting and drive detection
+    cpx  #$20    ;ID bytes for ProDOS and the
+    ldy  #$00    ; Apple Autostart ROM
+    cpx  #$03    ;
+    ldx  #$3C    ; this one for older II's
+    bne  badpage ; why are we booting to this page?
+jumpbank2:
+    sta  $bffb,x ; this is called to switch between rom banks
+
+badpage:
+    ldy	 #<card1msg
+writecard1msg:
+    lda	 (buflo),y
+    beq  getcard1key
+    sta	 $750-<msg,y
+    iny
+    bne  writecard1msg
+
+getcard1key:
+    lda  $c000
+    bpl  getcard1key
+    sta  $c010
+    cmp  #'!'+128
+    bne  notex1
+    sta  $750+20
+    lda  #$ff
+    bne  storevol1
+notex1:
+    cmp  #'0'+128
+    bcc  getcard1key
+    cmp  #'9'+128+1
+    bcs  getcard1key
+    sta  $750+20
+    and  #$0f
+storevol1:
+    sta  blklo
+
+    ldy	 #<card2msg
+writecard2msg:
+    lda	 (buflo),y
+    beq  getcard2key
+    sta	 $7D0-<msg,y
+    iny
+    bne  writecard2msg
+insidejump:
+    beq  jumpbank2    ; should never fall through here from previous instruction
+    
+getcard2key:
+    lda  $c000
+    bpl  getcard2key
+	sta  $c010
+    cmp  #'0'+128
+    bcc  getcard2key
+    cmp  #'9'+128+1
+    bcs  getcard2key
+    sta  $7D0+20
+    and  #$0f
+    sta  blkhi
+
+    lda  #$fa
+    sta  $BFFB,x     ; write to 82C55 mode register (mode 2 reg A, mode 0 reg B)
+    lda  #4          ; set command=4
+    sta  command
+
+    ldy	 #$ff        ; lets send the command bytes directly to the Arduino
+    lda  #$ac        ; send this byte first as a magic byte
+    bne  pcomsend
+pcombyte:
+    lda  command,y   ; get byte
+pcomsend:
+    sta  $BFF8,x     ; push it to the Arduino
+pcombyte2:
+    lda  $BFFA,x     ; get port C
+    bpl  pcombyte2    ; wait until its received (OBFA is high)
+    iny
+    cpy  #$06
+    bne  pcombyte     ; send next byte
+
+pwaitresult:
+    lda  $BFFA,x     ; wait until there's a byte available
+    and  #$20  
+    beq  pwaitresult
+    
+    lda  $BFF8,x     ; get the byte
+    lda  #0
+    beq  insidejump  ; try to reboot
+card1msg:   aschi   "CARD 1 (0-9,!):"
+end1msg:
+.byte    0
+card2msg:   aschi   "CARD 2 (0-9):"
+end2msg:
+.byte    0
+
+.repeat	255-<end2msg
+.byte 0
+.endrepeat
+
+
